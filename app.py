ce2f42e6099443232472f4a0f63cdcd0729f47bf
bharatadk <bharatadk.on@gmail.com>
2022-08-08 14:42:13 +0545

    de

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, render_template, request,url_for
 
 
@@ -6,7 +8,40 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template("index.html")
+    
+@app.route("/runcode", methods=['POST'])
+def runcode():
+    print('indise runcode')
+    if request.method == "POST":
+        codeareadata = request.form['codearea']
+        try:
+            print('insede try')
+            #save original standart output reference
 
+            original_stdout = sys.stdout
+            sys.stdout = open('file.txt', 'w') #change the standard output to the file we created
+
+            #execute code
+
+            exec(codeareadata)  #example =>   print("hello world")
+
+            sys.stdout.close()
+
+            sys.stdout = original_stdout  #reset the standard output to its original value
+
+            # finally read output from file and save in output variable
+
+            output = open('file.txt', 'r').read()
+
+        except Exception as e:
+            # to return error in the code
+            sys.stdout = original_stdout
+            output = e
+
+
+    #finally return and render index page and send codedata and output to show on page
+
+    return render_template('index.html', code=codeareadata , output=output)
 
 if __name__ =='__main__':
 	app.run()
